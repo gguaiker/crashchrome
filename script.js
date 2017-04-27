41bd98607401fc544d6b00918f516a07931755b9
TeamIlluminati <twinshack@gmail.com>
2017-04-27 15:57:37 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function OpenInNewTab(url) {
-  var win = window.open(url, '_blank');
+function OpenInNewTab('http://www.3dtuning.com/en-US/tuning/subaru/impreza/sedan-2005') {
+  var win = window.open('http://www.3dtuning.com/en-US/tuning/subaru/impreza/sedan-2005', '_blank');
   win.focus();
 }
